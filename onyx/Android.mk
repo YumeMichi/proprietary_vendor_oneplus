205fd9a416f10565b14acba23e88d8a1d0c86609
YumeMichi <do4suki@gmail.com>
2017-10-12 01:18:40 +0000

    onyx: Update Widevine DRM blobs from TheMuppets
    Change-Id: I6cc3eea5f19467ef18d19159d1024b6eac2df255
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- onyx/Android.mk
+++ onyx/Android.mk
@@ -95,16 +95,6 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.google.widevine.software.drm
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/framework/com.google.widevine.software.drm.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcnvitems
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/framework/qcnvitems.jar
