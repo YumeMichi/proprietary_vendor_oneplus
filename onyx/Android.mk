cf6a289d89d43d8dc2b4e13cc750d0780cd4d88f
YumeMichi <do4suki@gmail.com>
2018-03-09 17:55:29 +0800

    onyx: Move qcrilmsgtunnel back to /system
     * priv-app doesn't exist in /vendor
    
    Change-Id: I12ddadf3b407f2ef19c4ac45688c983e09f69858
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- onyx/Android.mk
+++ onyx/Android.mk
@@ -78,14 +78,13 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/vendor/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
